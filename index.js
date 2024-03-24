f27684e5dd1f0bc8bcadfc2c70b6c25e32883398
salman-AS <salmans581@gmail.com>
2024-03-24 20:26:16 +0530

    .

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ app.use(express.json())
 const morgan = require('morgan')
 // app.use(morgan('tiny'))
 morgan.token('body', function (req, res) { return `${JSON.stringify(req.body)}`})
-
 app.use(morgan(':method :url :status :res[content-length] - :response-time :body'))
 
 const cors = require('cors')
