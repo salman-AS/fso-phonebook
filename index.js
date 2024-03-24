ea5af8be3e7988be6fb63beb40034c63e7d549df
salman-AS <salmans581@gmail.com>
2024-03-24 19:39:42 +0530

    init

---
+++ index.js
@@ -0,0 +1,113 @@
+const express = require('express')
+const app = express()
+app.use(express.json())
+
+const morgan = require('morgan')
+// app.use(morgan('tiny'))
+morgan.token('body', function (req, res) { return `${JSON.stringify(req.body)}`})
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time :body'))
+
+const cors = require('cors')
+app.use(cors())
+
+
+// const requestLogger = (request, response, next) => {
+//     console.log('Method:', request.method)
+//     console.log('Path:  ', request.path)
+//     console.log('Body:  ', request.body)
+//     console.log('---')
+//     next()
+// }
+// app.use(requestLogger)
+
+const generateId = () => {
+    const maxId = persons.length > 0 ? Math.max(...persons.map(p => p.id)) : 0
+    return maxId + 1
+}
+
+let persons = [
+    {
+        "id": 1,
+        "name": "Arto Hellas",
+        "number": "040-123456"
+    },
+    {
+        "id": 2,
+        "name": "Ada Lovelace",
+        "number": "39-44-5323523"
+    },
+    {
+        "id": 3,
+        "name": "Dan Abramov",
+        "number": "12-43-234345"
+    },
+    {
+        "id": 4,
+        "name": "Mary Poppendieck",
+        "number": "39-23-6423122"
+    },
+    {
+        "id": 5,
+        "name": "Abramov",
+        "number": "12-43-234345"
+    }
+]
+
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
+
+app.get('/api/persons', (request, response) => {
+    response.json(persons)
+})
+
+app.get('/info', (request, response) => {
+    response.send(`<p>Phonebook has info for ${persons.length} people<p><p>${Date()}</p>`)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const person = persons.find(person => person.id === id)
+    if (person)
+        response.json(person)
+    else
+        response.status(404).end()
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const person = persons.find(person => person.id === id)
+    if (person) {
+        persons = persons.filter(person => person.id !== id)
+        response.status(204).end()
+    }
+    else
+        response.status(404).end()
+})
+
+app.post('/api/persons', (request, response) => {
+    const body = request.body
+    let person = persons.find(person => person.name === body.name)
+    if (!body.name || !body.number) {
+        response.status(400).json({ error: 'The name or number is missing' })
+    }
+    else if (!person) {
+        person = {
+            id: generateId(),
+            name: body.name,
+            number: body.number
+        }
+        persons = persons.concat(person)
+        response.json(person)
+    }
+    else
+        response.status(400).json({ error: 'The name already exists in the phonebook' })
+})
+
+// const unknownEndpoint = (request, response) => {
+//     response.status(404).send({ error: 'unknown endpoint' })
+// }
+// app.use(unknownEndpoint)
+
