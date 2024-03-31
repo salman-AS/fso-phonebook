183dff2dfdc3bd6131ba29cd16dbe5e9d998ea42
salman-AS <salmans581@gmail.com>
2024-03-31 21:44:30 +0530

    ex 3.21

--- index.js
+++ index.js
@@ -90,14 +90,14 @@ app.put('/api/persons/:id', (request, response, next) => {
         name: body.name,
         number: body.number,
     }
-    Person.findByIdAndUpdate(request.params.id, person, { new: true })
+    Person.findByIdAndUpdate(request.params.id, person, { new: true, runValidators: true, context: 'query' })
         .then(updatedPerson => {
             response.json(updatedPerson)
         })
         .catch(error => next(error))
 })
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
     const body = request.body
     if (body.name === undefined || body.number === undefined) {
         return response.status(400).json({ error: 'name or number is missing' })
@@ -110,6 +110,7 @@ app.post('/api/persons', (request, response) => {
         .then(savedPerson => {
             response.json(savedPerson)
         })
+        .catch(error => next(error))
 })
 
 const unknownEndpoint = (request, response) => {
@@ -121,6 +122,8 @@ const errorHandler = (error, request, response, next) => {
     console.error(error.message)
     if (error.name === 'CastError') {
         return response.status(400).send({ error: 'malformatted id' })
+    } else if (error.name === 'ValidationError') {
+        return response.status(400).json({ error: error.message })
     }
     next(error)
 }
